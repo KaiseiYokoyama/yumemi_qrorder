7809b42ff44687483078edb762c5c1d9e2f89bf5
k_yokoyama <yokoyama.kaisei.sm@alumni.tsukuba.ac.jp>
2021-09-14 16:22:57 +0900

    restaurantsテーブルのマイグレーションを記述

---
+++ database/migrations/2021_09_14_064534_create_restaurants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            // 店舗名
+            $table->string('name');
+            // 課金が切れる日時
+            $table->date('expired_date');
+            // hash化したパスワード
+            $table->string('pass_hash');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
