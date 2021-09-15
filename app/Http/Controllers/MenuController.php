554f4580b1eef85fca8b07f42ae525067a23817c
k_yokoyama <yokoyama.kaisei.sm@alumni.tsukuba.ac.jp>
2021-09-15 16:52:42 +0900

    MenuController->getAll(): お客さんのいる店だけのメニューを出す
    以前は全ての店の全てのメニューを出していた

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Menu;
+use App\Models\Party;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class MenuController extends Controller
 {
@@ -13,9 +16,20 @@ class MenuController extends Controller
     /**
      *
      */
-    public function getAll(): Collection
+    public function getAll(Request $request): Collection
     {
-        $model_query = Menu::query();
+        $session_secret = $request->cookie('session_secret');
+        $party = Party::query()
+            ->where('uuid', $session_secret)
+            ->first();
+
+        if ($party == null) {
+            throw new HttpException(Response::HTTP_FORBIDDEN);
+        }
+
+
+        $model_query = Menu::query()
+            ->where('restaurant_id', $party->value('restaurant_id'));
         $menus = $model_query->get();
 
         return $menus;
