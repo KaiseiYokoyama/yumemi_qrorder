a05ac15d9eea0a5df36305019ad06166c511c16d
k_yokoyama <yokoyama.kaisei.sm@alumni.tsukuba.ac.jp>
2021-09-15 15:05:17 +0900

    GET /api/menuでメニュー一覧を取得する
    認可と組み合わせて、そのお客さんのいる店舗のメニューだけにしたい

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Menu;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    //
+    // TODO: リソースコントローラを使ってリファクタリング
+
+    /**
+     *
+     */
+    public function getAll(): Collection
+    {
+        $model_query = Menu::query();
+        $menus = $model_query->get();
+
+        return $menus;
+    }
 }
