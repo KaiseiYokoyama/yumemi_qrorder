8cfeeccdf3f5dc696b90e809f28070606c36192c
k_yokoyama <yokoyama.kaisei.sm@alumni.tsukuba.ac.jp>
2021-09-17 17:36:55 +0900

    feat: CreateOrderRequest.php 新規注文リクエストのバリデーションを実装

---
+++ app/Http/Requests/CreateOrderRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'menu_ids' => 'required|array',
+            'menu_ids.*' => 'int|min:1',
+        ];
+    }
+}
